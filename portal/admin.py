062e263cf164a05de41d045c8d3d17f3e688d436
Apple <apple@Apples-MacBook-Pro.local>
2013-07-09 16:12:51 -0400

    admin change

--- portal/admin.py
+++ portal/admin.py
@@ -2,20 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-
-from portal.models import project
-from portal.models import user_project
 from portal.models import article
 from portal.models import user_extra_field
 
-class portal_projects_Admin(admin.ModelAdmin):                             
-    fieldsets = [
-    ('Title', {'fields':['title', 'authors']}), 
-    ]
-
-admin.site.register(project, portal_projects_Admin)
-
-
+# Patient Article Admin
 class portal_article_Admin(admin.ModelAdmin):
     fieldsets = [
     ('Title', {'fields':['title', 'authors', 'text']}),
@@ -24,6 +14,7 @@ admin.site.register(article, portal_article_Admin)
 
 #--------------------------------------------------------
 
+# User Extra Feild Admin
 class user_extra_field_Admin(admin.ModelAdmin):
     fieldsets = [
     ('Title', {'fields':['user', 'gender']}),
@@ -33,20 +24,9 @@ admin.site.register(user_extra_field, user_extra_field_Admin)
 class UserExtraInline(admin.StackedInline):
     model = user_extra_field
     extra = 0
-
-class portal_user_projects_Admin(admin.ModelAdmin):                             
-    fieldsets = [
-    ('Title', {'fields':['title', 'authors', 'user']}), 
-    ]
-
-admin.site.register(user_project, portal_user_projects_Admin)
-
-class ProjectInline(admin.StackedInline):
-    model = user_project
-    extra = 0
-
+# User Admin
 class UserAdmin(UserAdmin):
-    inlines = [ProjectInline, UserExtraInline]
+    inlines = [UserExtraInline]
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
